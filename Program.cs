efd72e1325a4ebbc694d6f40a5070a1886292f68
Vonfagelsang <89455450+Vonfagelsang@users.noreply.github.com>
2022-08-25 12:03:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿class Program{
+    class Bil{
+        private string modell;
+        private string årsmodell;
+        private string vikt;
+        public void setModell(string m) { modell = m; }
+        public string getModell() { return modell; }
+        public void setÅr(string å) { årsmodell = å; }
+        public string getÅr() { return årsmodell; }
+        public void setVikt(string v) { vikt = v; }
+        public string getVikt() { return vikt; }
+
+    }
+    public static void Main(){
+        Bil b = new Bil();
+        b.setModell("Modell: Volvo");
+        b.setÅr("År: V70");
+        Console.WriteLine("Skriv vikt på bilen");
+        b.setVikt("Vikt: " + Console.ReadLine() + "Kg");
+        Console.Clear();
+
+
+        string[] Bilar = { b.getModell(), b.getÅr(), b.getVikt()};
+        List<string> bil = new List<string>();
+        bil.AddRange(Bilar);
+        Console.WriteLine("Bil info:");
+        Console.WriteLine("");
+        foreach (string a in bil)
+        {
+            Console.WriteLine(a);
+        }
+    }
+}
+    

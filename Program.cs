94730221e02f250de4cff4dbd1eb342a5da20427
Vonfagelsang <89455450+Vonfagelsang@users.noreply.github.com>
2022-09-01 12:12:28 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,34 +1,42 @@
 ﻿class Program{
     class Bil{
-        private string modell;
-        private string årsmodell;
-        private string vikt;
-        public void setModell(string m) { modell = m; }
-        public string getModell() { return modell; }
-        public void setÅr(string å) { årsmodell = å; }
-        public string getÅr() { return årsmodell; }
-        public void setVikt(string v) { vikt = v; }
-        public string getVikt() { return vikt; }
-
+        public string modell;
+        public string årsmodell;
+        public string vikt;
+        public Bil(string m, string å, string v){
+            modell = m;
+            årsmodell = å;
+            vikt = v;
+            modell = "Modell: " + Console.ReadLine();
+            årsmodell = "År: " + Console.ReadLine();
+            vikt = ("Vikt: " + Console.ReadLine() + "Kg");
+        }
+    }
+    class Fordonslista{
+        public Fordonslista(int skrivaUt = 1){
+            Bil b = new Bil("", "", "");
+            List<Bil> bil = new List<Bil>();
+            bil.Add(b);
+            foreach (Bil element in bil)
+            {
+                Console.Clear();
+                Console.WriteLine("Bil info:");
+                Console.WriteLine("");
+                Console.WriteLine(element.modell);
+                Console.WriteLine(element.årsmodell);
+                Console.WriteLine(element.vikt);
+            }
+            
+            
+        }
     }
     public static void Main(){
-        Bil b = new Bil();
-        b.setModell("Modell: Volvo");
-        b.setÅr("År: V70");
-        Console.WriteLine("Skriv vikt på bilen");
-        b.setVikt("Vikt: " + Console.ReadLine() + "Kg");
         Console.Clear();
-
-
-        string[] Bilar = { b.getModell(), b.getÅr(), b.getVikt()};
-        List<string> bil = new List<string>();
-        bil.AddRange(Bilar);
-        Console.WriteLine("Bil info:");
-        Console.WriteLine("");
-        foreach (string a in bil)
-        {
-            Console.WriteLine(a);
+        int skrivaUt = 1;
+        while(skrivaUt != 3){
+        Console.WriteLine("Skriv modell, år och vikt på bilen");
+        Fordonslista a = new Fordonslista();
+        skrivaUt += 1;
         }
     }
 }
-    
